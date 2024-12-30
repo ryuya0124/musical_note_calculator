3c7cf7ede9b6cc9f8c0f95ae6c8d9690e92e247b
ryuya <ryuya1adgjmptw@gmail.com>
2024-12-30 12:34:24 +0900

    Initial commit

---
+++ android/app/src/main/java/io/github/ryuya0124/musical/note/calculator/musical_note_calculator/MainActivity.java
@@ -0,0 +1,6 @@
+package io.github.ryuya0124.musical.note.calculator.musical_note_calculator;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
