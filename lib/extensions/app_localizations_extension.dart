ba04c9d37e8490784778e70e21c74b9123fe4d1e
ryuya <ryuya1adgjmptw@gmail.com>
2025-01-02 19:12:47 +0900

    音符を異なるBPMへ換算するページの計算を実装

--- lib/extensions/app_localizations_extension.dart
+++ lib/extensions/app_localizations_extension.dart
@@ -24,47 +24,4 @@ extension AppLocalizationsExtension on AppLocalizations {
     };
     return translations[key] ?? key; // 翻訳がない場合はキーを表示
   }
-
-  String get(String key) {
-    switch (key) {
-      case 'maxima':
-        return maxima;
-      case 'longa':
-        return longa;
-      case 'double_whole_note':
-        return double_whole_note;
-      case 'whole_note':
-        return whole_note;
-      case 'half_note':
-        return half_note;
-      case 'fourBeatsThreeConsecutive':
-        return fourBeatsThreeConsecutive;
-      case 'dotted_half_note':
-        return dotted_half_note;
-      case 'quarter_note':
-        return quarter_note;
-      case 'dotted_quarter_note':
-        return dotted_quarter_note;
-      case 'eighth_note':
-        return eighth_note;
-      case 'dotted_eighth_note':
-        return dotted_eighth_note;
-      case 'twoBeatsTriplet':
-        return twoBeatsTriplet;
-      case 'sixteenth_note':
-        return sixteenth_note;
-      case 'dotted_sixteenth_note':
-        return dotted_sixteenth_note;
-      case 'oneBeatTriplet':
-        return oneBeatTriplet;
-      case 'oneBeatQuintuplet':
-        return oneBeatQuintuplet;
-      case 'oneBeatSextuplet':
-        return oneBeatSextuplet;
-      case 'thirty_second_note':
-        return thirty_second_note;
-      default:
-        return 'Unknown key: $key';
-    }
-  }
 }
