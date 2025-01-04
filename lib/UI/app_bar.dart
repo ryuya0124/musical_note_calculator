1f3ffa1f932e4e91737f5143b8743b15408ebed6
ryuya <ryuya1adgjmptw@gmail.com>
2025-01-04 21:55:37 +0900

    アプリバーの色変だったから変えた(まだなんか変)

--- lib/UI/app_bar.dart
+++ lib/UI/app_bar.dart
@@ -23,12 +23,16 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
 
     final colorScheme = Theme.of(context).colorScheme;
 
+    // テキストの色をダークテーマ・ライトテーマに合わせて設定
+    final titleTextColor = colorScheme.primary; // タイトルのテキスト色
+    final tabTextColor = colorScheme.onSurface; // タブ名のテキスト色
+
     // 設定画面以外
     if (selectedIndex != 10) {
       return AppBar(
         automaticallyImplyLeading: false,
-        backgroundColor: colorScheme.primary,
-        foregroundColor: colorScheme.onPrimary,
+        backgroundColor: colorScheme.onPrimary, // 背景色をテーマの onPrimary に
+        foregroundColor: colorScheme.primary, // アイコンの色をテーマの primary に
         title: Row(
           children: [
             Text(
@@ -36,14 +40,14 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
               style: Theme.of(context)
                   .textTheme
                   .titleLarge
-                  ?.copyWith(color: colorScheme.onPrimary),
+                  ?.copyWith(color: titleTextColor), // タイトルのテキスト色
             ),
             SizedBox(width: 8), // タイトルとタブ名の間隔を調整
             Text(
               tabNames[selectedIndex], // 現在のタブ名を表示
               style: Theme.of(context).textTheme.titleMedium?.copyWith(
-                color: colorScheme.onPrimary,
                 fontWeight: FontWeight.normal,
+                color: tabTextColor, // タブ名のテキスト色
               ),
             ),
           ],
@@ -57,29 +61,28 @@ class AppBarWidget extends StatelessWidget implements PreferredSizeWidget {
                 MaterialPageRoute(builder: (context) => SettingsPage()),
               );
             },
-            color: colorScheme.onPrimary,
+            color: colorScheme.primary, // アイコンの色
           ),
         ],
       );
-
-      // 設定画面
     } else {
+      // 設定画面
       return AppBar(
-        backgroundColor: colorScheme.primary,
-        foregroundColor: colorScheme.onPrimary,
+        backgroundColor: colorScheme.onPrimary, // 背景色をテーマの onPrimary に
+        foregroundColor: colorScheme.primary, // アイコンの色をテーマの primary に
         title: Text(
           AppLocalizations.of(context)!.settings,
           style: Theme.of(context)
               .textTheme
               .titleLarge
-              ?.copyWith(color: colorScheme.onPrimary),
+              ?.copyWith(color: titleTextColor), // 設定画面のタイトル色
         ),
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
             Navigator.pop(context); // 戻るボタン
           },
-          color: colorScheme.onPrimary, // 戻るアイコンの色を設定
+          color: colorScheme.primary, // 戻るアイコンの色
         ),
       );
     }
