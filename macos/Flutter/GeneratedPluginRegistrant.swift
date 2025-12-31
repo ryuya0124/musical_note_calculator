fd91aa931f7f9bc9e186c9c8565742657fe6045e
ryuya <ryuya1adgjmptw@gmail.com>
2025-12-31 19:51:14 +0900

    feat: `path_provider_foundation`プラグインを追加し、PodfileとXcode設定ファイルを更新しました。

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -8,6 +8,7 @@ import Foundation
 import dynamic_color
 import metronome
 import package_info_plus
+import path_provider_foundation
 import shared_preferences_foundation
 import url_launcher_macos
 
@@ -15,6 +16,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   MetronomePlugin.register(with: registry.registrar(forPlugin: "MetronomePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
